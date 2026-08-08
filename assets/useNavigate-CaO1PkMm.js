d13c56cbb8069bd135700c2a30d258c639db97de
KonyaGo Deploy <konyago@users.noreply.github.com>
2026-08-08 20:40:49 +0000

    deploy: production static site

---
+++ assets/useNavigate-CaO1PkMm.js
@@ -0,0 +1,1 @@
+import{U as e,V as t,i as n}from"./createLucideIcon-Bqz0ZtiK.js";var r=e(t(),1);function i(e){let t=n();return r.useCallback(n=>t.navigate({...n,from:n.from??e?.from}),[e?.from,t])}export{i as t};
